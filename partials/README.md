122ff915dfc2d348c79f390c8bf77e7c5ce076dc
Eduardo Andrés Castillo Perera <eduardoandrescp@outlook.com>
2019-02-19 01:58:12 -0600

    Se agregaron varios archivos, entre ellos el login y unos partials. Se hicieron varios commits internos en mi branch que no se muestran aqui

---
+++ partials/README.md
@@ -0,0 +1,6 @@
+# Objetivo de este folder
+Este folder es para poner partials y que en un futuro cuando
+tengamos que usar PHP ya no tengamos que crearlos. Por ahora no hacen nada. 
+Pero ya estan por si se les ofrece
+
+:D
---
+++ partials/bottom.php
@@ -0,0 +1,44 @@
+
+<!--El Footer de la Página-->
+<footer class="page-footer grey darken-3">
+    <div class="container grey darken-3">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Cáritas Querétaro: </h5>
+                <p class="grey-text text-lighten-4">
+                    Vergara #35-A Col. Centro <br>
+                    Tels: 212 33 19 / 212 35 96 <br>
+                    Horario de oficinas: Lunes a <br>
+                    Viernes de 8am a 4pm</p>
+
+
+            </div>
+            <div class="col l3 s12"></div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Buscanos en:</h5>
+                <ul>
+                    <li><a class="white-text" href="#!"><img src="icons/174834-social-media-logos/png/001-youtube.png" alt="Youtube" height="25px" width="25px">  Youtube</a></li>
+                    <li><a class="white-text" href="#!"><img src="icons/174834-social-media-logos/png/007-vimeo.png" alt="Vimeo" height="25px" width="25px">  Vimeo</a></li>
+                    <li><a class="white-text" href="#!"><img src="icons/174834-social-media-logos/png/029-instagram.png" alt="instagram" height="25px" width="25px">  Instagram</a></li>
+                    <li><a class="white-text" href="#!"><img src="icons/174834-social-media-logos/png/036-facebook.png" alt="facebook" height="25px" width="25px"></a>  Facebook</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            Made by <a class="brown-text text-lighten-3" href="#">WeByte</a> Iconos diseñados por <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a
+                    href="https://www.flaticon.es/" title="Flaticon">www.flaticon.com</a> con licencia <a
+                    href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+        </div>
+    </div>
+</footer>
+
+<!--  Scripts-->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+
+</body>
+
+</html>
